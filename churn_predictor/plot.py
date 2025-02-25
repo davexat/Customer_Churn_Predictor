68fee8a7608bc636e9fb67631ff47376a9737860
davexat <yisuscristh20@gmail.com>
2025-02-24 19:03:55 -0500

    chore: improving plot functions

--- churn_predictor/plot.py
+++ churn_predictor/plot.py
@@ -3,24 +3,70 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 
-def summary_cat_var(df, column, gbe=False, sbs=False):
+def init():
+    sns.set_style('whitegrid')
+
+def summary_cat_var(df, column, gbe=False, sbs=False, summ=True):
+    df_gb = plot_barplot(df, column, gbe, sbs)
+    if summ: print(df_gb)
+
+def plot_barplot(df, column, gbe=False, sbs=False):
     df_gb = df.groupby('Exited') if gbe else df
     df_gb = df_gb[column].value_counts(normalize=True)
     df_freq = df_gb.reset_index()
 
-    sns.catplot(data=df_freq, 
-                kind='bar', x=column, 
-                y='proportion',
-                palette = {0: '#1f77b4', 1: '#d62728'} if gbe else None,
-                hue = 'Exited' if gbe else None,
-                col = 'Exited' if sbs and gbe else None,
-                col_order = [0,1] if sbs and gbe else None,
-                height=5, aspect=1)
-    
+    sns.catplot(
+        data=df_freq, 
+        kind='bar', x=column, 
+        y='proportion',
+        palette = {0: '#1f77b4', 1: '#d62728'} if gbe else None,
+        hue = 'Exited' if gbe else None,
+        col = 'Exited' if sbs and gbe else None,
+        col_order = [0,1] if sbs and gbe else None,
+        height=5, 
+        aspect=1 if sbs else 2
+    )
     plt.show()
-    print(df_gb)
+    return df_gb
 
-def plot_horizontal_boxplot(df, columns):
+def summary_num_var(df, column, gbe=False, sbs=False, summ=True, bins=None):
+    plot_histogram(df, column, gbe, sbs)
+    plot_boxplot(df, column, gbe, sbs)
+    info = df.groupby('Exited') if gbe else df
+    if summ:
+        print(info[column].describe())
+
+def plot_boxplot(df, column, gbe=False, sbs=False):
+    sns.catplot(
+        data=df, 
+        kind='box', x=column,
+        palette = {0: '#1f77b4', 1: '#d62728'} if gbe else None,
+        hue = 'Exited' if gbe else None, 
+        col = 'Exited' if sbs and gbe else None, 
+        col_order=[0,1] if sbs and gbe else None, 
+        height=2, 
+        aspect= 2.5 if sbs else 5
+    )
+    plt.show()
+
+def plot_histogram(df, column, gbe=False, sbs=False):
+    sns.displot(
+        data=df,
+        kind='hist',
+        x=column,
+        palette={0: "#1f77b4", 1: "#d62728"} if gbe else None,
+        hue='Exited' if gbe else None,
+        col='Exited' if sbs and gbe else None,
+        col_order=[0,1] if sbs and gbe else None,
+        stat="probability",
+        common_norm=False,
+        kde=True,
+        height=5, 
+        aspect=1 if sbs else 2
+    )
+    plt.show()
+
+def plot_horizontal_boxplot_x(df, columns):
     """
     Plot a horizontal boxplot for a specified column in a DataFrame.
     
