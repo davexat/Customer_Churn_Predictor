c4707c7f9f33d98068081e06eb4b1e17285e3401
davexat <yisuscristh20@gmail.com>
2025-04-25 19:18:24 -0500

    chore: univariate analysis

--- churn_predictor/plot.py
+++ churn_predictor/plot.py
@@ -7,41 +7,33 @@ import numpy as np
 def init():
     sns.set_style('whitegrid')
 
-def summary_cat_var(df, column, gbe=False, sbs=False, summ=True):
-    df_gb = plot_barplot(df, column, gbe, sbs)
-    if summ: print(df_gb)
-
-def plot_barplot(df, column, gbe=False, sbs=False):
-    if isinstance(column, list):
-        for col in column: plot_barplot(df, col, gbe, sbs)
-        return
-    df_gb = df.groupby('Exited') if gbe else df
-    df_gb = df_gb[column].value_counts(normalize=True)
-    df_freq = df_gb.reset_index()
-
-    sns.catplot(
-        data=df_freq, 
-        kind='bar', x=column, 
-        y='proportion',
-        palette = {0: '#1f77b4', 1: '#d62728'} if gbe else None,
-        hue = 'Exited' if gbe else None,
-        col = 'Exited' if sbs and gbe else None,
-        col_order = [0,1] if sbs and gbe else None,
-        height=5, 
-        aspect=1 if sbs else 2,
-        edgecolor='black'
-    )
-    plt.title(f"Barplot for {column}")
-    plt.show()
-    return df_gb
-
-def summary_num_var(df, column, gbe=False, sbs=False, summ=True, bins=None):
+def summary_num_var(df, column, gbe=False, sbs=False):
     plot_histogram(df, column, gbe, sbs)
     plot_boxplot(df, column, gbe, sbs)
     info = df.groupby('Exited') if gbe else df
-    if summ:
-        print(info[column].describe())
+    print_skewness_kurtosis(df, column)
+    print('--'*20)
+    print(info[column].describe())
+    print('--'*20)
+    kolmogorov_norm_test(df[column])
 
+def print_skewness_kurtosis(df, column): 
+    print(f"Skewness of {column}: {stats.skew(df[column])}")
+    print(f"Kurtosis of {column}: {stats.kurtosis(df[column])}")
+
+def kolmogorov_norm_test(data):
+    data_std = (data - data.mean()) / data.std()
+    stat, p = stats.kstest(data_std, 'norm')
+    print("Kolmogorov-Smirnov Test for Normality:")
+    print(f"Statistic = {stat:.4f}")
+    print(f"p-value   = {p:.4f}")
+    if p < 0.05:
+        print("\nSince the p-value is less than 0.05, we reject the null hypothesis.")
+        print("The data does not follow a normal distribution.")
+    else:
+        print("\nSince the p-value is greater than 0.05, we fail to reject the null hypothesis.")
+        print("The data may follow a normal distribution.")
+    
 def plot_boxplot(df, column, gbe=False, sbs=False):
     if isinstance(column, list):
         for col in column: plot_boxplot(df, col, gbe, sbs)
@@ -80,6 +72,35 @@ def plot_histogram(df, column, gbe=False, sbs=False):
     )
     plt.title(f"Histogram for {column}")
     plt.show()
+
+def summary_cat_var(df, column, gbe=False, sbs=False, summ=True):
+    df_gb = plot_barplot(df, column, gbe, sbs)
+    if summ: print(df_gb)
+
+def plot_barplot(df, column, gbe=False, sbs=False):
+    if isinstance(column, list):
+        for col in column: plot_barplot(df, col, gbe, sbs)
+        return
+    df_gb = df.groupby('Exited') if gbe else df
+    df_gb = df_gb[column].value_counts(normalize=True)
+    df_freq = df_gb.reset_index()
+
+    sns.catplot(
+        data=df_freq, 
+        kind='bar', x=column, 
+        y='proportion',
+        palette = {0: '#1f77b4', 1: '#d62728'} if gbe else None,
+        hue = 'Exited' if gbe else None,
+        col = 'Exited' if sbs and gbe else None,
+        col_order = [0,1] if sbs and gbe else None,
+        height=5, 
+        aspect=1 if sbs else 2,
+        edgecolor='black'
+    )
+    plt.title(f"Barplot for {column}")
+    plt.show()
+    return df_gb
+
 
 def plot_correlation_heatmap(df, method='pearson'):
     corr = df.corr(method)
